005894cf28920a63cebde4a613e88a535fddc516
Andrew Tapper <andrew@tapper.codes>
2022-09-28 13:53:54 -0500

    :art: organize file structure

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import router from './router'
 
 const app = express()
 
@@ -8,4 +9,6 @@ app.get('/', (req, res) => {
   res.json({ message: 'hello from server' })
 })
 
-module.exports = app
+app.use('/api', router)
+
+export default app
